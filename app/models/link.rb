d98999ef468b157f1db2a0d49c878e7b85837cea
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-06-06 12:22:36 -0600

    Simple title validation - url not validated

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,7 @@
 class Link < ActiveRecord::Base
   belongs_to :user
+
+  validates :title, presence: true
+  # validates :url, :url => true
+  # validates :url, format: { with: URI.regexp }, if: Proc.new { |a| a.url.present? }
 end
