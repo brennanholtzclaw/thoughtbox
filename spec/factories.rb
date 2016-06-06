ebc3ddbe42b54e50832b360e1597ea33d3758a98
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-06-06 14:02:21 -0600

    alter link index display and set up factory for next test

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,26 @@
 FactoryGirl.define do
+
+  sequence :url, %w(1 2 3 4 5 6).cycle do |number|
+    "http://www.example.com/#{number}"
+  end
+
+  sequence :title, %w(1 2 3 4 5 6).cycle do |number|
+    "Link Title #{number}"
+  end
+
   factory :link do
-    url "http://www.example.com"
-    title "MyString"
+    url
+    title
     read false
     user nil
   end
+
   factory :user do
     email "user@user.com"
     password "password"
+
+    factory :user_with_links do
+      links { create_list(:link, 3) }
+    end
   end
 end
