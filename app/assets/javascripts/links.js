1bf94f9d2bce70ac4d29527da94171798c53daef
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-06-06 16:44:58 -0600

    abandoning branch to implement some styling

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -1,11 +1,49 @@
 $(document).ready(function(){
-  var $body = $('body');
+  window.onload = fetchLinks;
   $("#mark-read").on("click", markRead);
   $("#mark-unread").on("click", markUnread);
-  $("#idea_filter_name").on("keyup", filterIdeas);
-  $body.on("click", "input#delete-idea", deleteIdea);
-  $body.on("click", "input#thumbs-up", thumbsUp);
-  $body.on("click", "input#thumbs-down", thumbsDown);
-  $body.on("blur", "h3.displayed-idea-title", editTitle);
-  $body.on("blur", "p.displayed-idea-body", editBody);
 });
+
+function markRead(){
+
+}
+
+function markUnread(){
+
+}
+
+function fetchLinks(){
+  $.ajax({
+    url: "api/v1/links",
+    method: "GET",
+    dataType: "json",
+    success: showLinks,
+    error: function(){
+      alert("Something went wrong");
+    }
+  });
+}
+
+function showLinks(results) {
+  var $wholeLink = $('.whole-link');
+  $wholeLink.html("");
+  results.forEach(function(link){
+    $wholeLink.prepend(linkDiv(link));
+  });
+}
+
+function prependLink(idea) {
+  $('.whole-link').prepend(linkDiv(idea));
+}
+
+function linkDiv(idea) {
+  return "<div class='idea true'>" +
+  idea.title +
+  "<br>" +
+  idea.url +
+  "<br>" +
+  "<input type='button' value='Mark as Read' class='btn btn-danger' id='mark-read'>  " +
+  "<input type='button' value='Edit this link' class='btn btn-warning' id='edit-link'>  " +
+  "</div>";
+// "<input type='button' value='Mark as Unread' class='btn btn-danger' id='mark-unread'>  " +
+}
