8b3f8b7d88c49d052cda80f6a10f0bd5bbd7ac78
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-06-06 17:00:53 -0600

    Add the most minimal style ever

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-  var $body = $('body');
-  $("#mark-read").on("click", markRead);
-  $("#mark-unread").on("click", markUnread);
-  $("#idea_filter_name").on("keyup", filterIdeas);
-  $body.on("click", "input#delete-idea", deleteIdea);
-  $body.on("click", "input#thumbs-up", thumbsUp);
-  $body.on("click", "input#thumbs-down", thumbsDown);
-  $body.on("blur", "h3.displayed-idea-title", editTitle);
-  $body.on("blur", "p.displayed-idea-body", editBody);
-});
+
