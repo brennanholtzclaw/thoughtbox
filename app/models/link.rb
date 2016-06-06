063b3b73d7b9726652fc5dde719d94abb614af63
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-06-06 13:23:10 -0600

    Implement and test adding new link

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,5 +2,5 @@ class Link < ActiveRecord::Base
   belongs_to :user
 
   validates :title, presence: true
-  # validates :url, :url => true
+  validates :url, :url => true
 end
--- spec/features/user_submits_link_spec.rb
+++ spec/features/user_submits_link_spec.rb
@@ -10,13 +10,44 @@ RSpec.feature "User can submit new links" do
       fill_in "Confirm password", with: "password"
       click_button "Login"
 
-      fill_in "Title", with: "Test link"
+      fill_in "Title", with: "Test Link"
       fill_in "Url", with: "http://www.example.com"
+      click_button "Submit Link"
 
-      expect(current_path).to eq links_path
       expect(page).to have_content("THOUGHTS FOUND HERE!")
       expect(page).to have_content("Test Link")
       expect(page).to have_content("http://www.example.com")
+    end
+  end
+
+  context "User enters invalid link or no title" do
+    scenario "User enters invalid Link" do
+      user = create(:user)
+      visit login_path
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "password"
+      fill_in "Confirm password", with: "password"
+      click_button "Login"
+
+      fill_in "Title", with: "Test Link"
+      fill_in "Url", with: "notalink"
+      click_button "Submit Link"
+
+      expect(page).to have_content("Url is not a valid URL")
+    end
+
+    scenario "User doesn't enter a title" do
+      user = create(:user)
+      visit login_path
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "password"
+      fill_in "Confirm password", with: "password"
+      click_button "Login"
+
+      fill_in "Url", with: "http://www.example.com"
+      click_button "Submit Link"
+
+      expect(page).to have_content("Title can't be blank")
     end
   end
 end
